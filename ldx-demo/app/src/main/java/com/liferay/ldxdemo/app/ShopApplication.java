8539a838830acf12e01a309f95b54764e62cf68c
Javier Gamarra <nhpatt@gmail.com>
2015-10-23 12:22:36 +0200

    beacon support for notifications with distance

--- ldx-demo/app/src/main/java/com/liferay/ldxdemo/app/ShopApplication.java
+++ ldx-demo/app/src/main/java/com/liferay/ldxdemo/app/ShopApplication.java
@@ -6,31 +6,36 @@ import android.util.Log;
 
 import com.liferay.ldxdemo.beacon.NotificationUtil;
 
+import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
+
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * @author Javier Gamarra
  */
 public class ShopApplication extends Application implements BeaconConsumer {
 
+	public static final String BEACON_LAYOUT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";
 	protected static final String TAG = "ShopApp";
-
+	private static final double MIN_DISTANCE = 2D;
+	private Date lastNotificationSent = new Date(0);
 	private BeaconManager beaconManager;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
 
-		beaconManager = BeaconManager.getInstanceForApplication(this);
-		// To detect proprietary beacons, you must add a line like below corresponding to your beacon
-		// type.  Do a web search for "setBeaconLayout" to get the proper expression.
-		beaconManager.getBeaconParsers().add(new BeaconParser().
-				setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
-		beaconManager.bind(this);
+		bindBeacon();
 	}
 
 	@Override
@@ -39,7 +44,6 @@ public class ShopApplication extends Application implements BeaconConsumer {
 			@Override
 			public void didEnterRegion(Region region) {
 				Log.i(TAG, "I just saw an beacon for the first time!");
-				NotificationUtil.sendNotification(getApplicationContext());
 			}
 
 			@Override
@@ -53,9 +57,50 @@ public class ShopApplication extends Application implements BeaconConsumer {
 			}
 		});
 
+
+		beaconManager.setRangeNotifier(new RangeNotifier() {
+			@Override
+			public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+				processNearBeacons(collection);
+			}
+		});
+
 		try {
-			beaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
+			Identifier beaconId = Identifier.fromUuid(UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"));
+			Region sth = new Region("shop", beaconId, null, null);
+			beaconManager.startMonitoringBeaconsInRegion(sth);
+			beaconManager.startRangingBeaconsInRegion(sth);
 		} catch (RemoteException e) {
+			Log.e(TAG, "Error reading beacons", e);
 		}
+	}
+
+	private void bindBeacon() {
+		beaconManager = BeaconManager.getInstanceForApplication(this);
+		beaconManager.getBeaconParsers().add(new BeaconParser().
+				setBeaconLayout(BEACON_LAYOUT));
+		beaconManager.bind(this);
+	}
+
+	private void processNearBeacons(Collection<Beacon> collection) {
+		for (Beacon beacon : collection) {
+			double distance = beacon.getDistance();
+			if (distance < MIN_DISTANCE) {
+
+				Log.e("Beacon close, at: ", String.valueOf(distance) + " meters");
+
+				if (lastNotificationSent.before(getTimeFiveMinutesAgo())) {
+					NotificationUtil.sendNotification(getApplicationContext());
+
+					lastNotificationSent = new Date();
+				}
+			}
+		}
+	}
+
+	private Date getTimeFiveMinutesAgo() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.MINUTE, -5);
+		return calendar.getTime();
 	}
 }

afdbbf01121f981d84abe8fc2a253f6ce046161d
Javier Gamarra <nhpatt@gmail.com>
2015-10-26 15:02:39 +0100

    remove useless menu

--- ldx-demo/app/src/main/java/com/liferay/ldxdemo/activities/MainActivity.java
+++ ldx-demo/app/src/main/java/com/liferay/ldxdemo/activities/MainActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -14,6 +12,7 @@ import com.liferay.ldxdemo.R;
 import com.liferay.mobile.screens.auth.login.LoginListener;
 import com.liferay.mobile.screens.auth.login.LoginScreenlet;
 import com.liferay.mobile.screens.context.User;
+
 /**
  * @author Javier Gamarra
  */
@@ -32,28 +31,6 @@ public class MainActivity extends AppCompatActivity implements LoginListener {
 		loginScreenlet.setListener(this);
 
 		setDefaultValuesForUserAndPassword();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
 	}
 
 	@Override

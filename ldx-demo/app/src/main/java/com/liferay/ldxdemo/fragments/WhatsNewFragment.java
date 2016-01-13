26ac4adacfaa9417f752fbd2aafc1f7c7b883bb8
jonaschoi <jonas.choi@liferay.com>
2016-01-13 14:58:27 -0800

    Implemented AssetListListener

--- ldx-demo/app/src/main/java/com/liferay/ldxdemo/fragments/WhatsNewFragment.java
+++ ldx-demo/app/src/main/java/com/liferay/ldxdemo/fragments/WhatsNewFragment.java
@@ -7,8 +7,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.liferay.ldxdemo.R;
+import com.liferay.mobile.screens.assetlist.AssetEntry;
+import com.liferay.mobile.screens.assetlist.AssetListListener;
+import com.liferay.mobile.screens.base.list.BaseListScreenlet;
 
-public class WhatsNewFragment extends AbstractAssetPubFragment {
+import java.util.List;
+
+public class WhatsNewFragment extends AbstractAssetPubFragment implements AssetListListener {
 
 	public static WhatsNewFragment newInstance() {
 
@@ -23,5 +28,35 @@ public class WhatsNewFragment extends AbstractAssetPubFragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.content_whats_new, container, false);
+	}
+
+	@Override
+	public void onListPageFailed(BaseListScreenlet source, int page, Exception e) {
+
+	}
+
+	@Override
+	public void onListPageReceived(BaseListScreenlet source, int page, List<AssetEntry> entries, int rowCount) {
+
+	}
+
+	@Override
+	public void onListItemSelected(AssetEntry element, View view) {
+
+	}
+
+	@Override
+	public void loadingFromCache(boolean success) {
+
+	}
+
+	@Override
+	public void retrievingOnline(boolean triedInCache, Exception e) {
+
+	}
+
+	@Override
+	public void storingToCache(Object object) {
+
 	}
 }
